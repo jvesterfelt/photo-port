afc463cdb6d8a38eee6ce5603f6fa92433c6abb5
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-09-02 12:20:07 -0600

    Added tests for the modal component.

--- src/components/PhotoList/index.jsx
+++ src/components/PhotoList/index.jsx
@@ -125,7 +125,7 @@ const PhotoList = ({ category }) => {
   const toggleModal = (image, i) => {
     // current photo
     setCurrentPhoto({ ...image, index: i });
-    setIsModalOpen(true);
+    setIsModalOpen(!isModalOpen);
   }
 
   
@@ -133,7 +133,7 @@ const PhotoList = ({ category }) => {
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={ toggleModal }/>}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
